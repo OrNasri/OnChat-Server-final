48000650ff7c43cdf45324f110d994a2ae5f658c
OrNasri <91970437+OrNasri@users.noreply.github.com>
2022-05-27 17:10:36 +0300

    Update Service.cs

--- onChat/Models/Service.cs
+++ onChat/Models/Service.cs
@@ -6,7 +6,7 @@
         {
             id = "or",
             nickname = "orush",
-            password = "1",
+            password = "Nn123456",
             image = "https://bootdey.com/img/Content/avatar/avatar3.png",
             contacts = new List<Contact>
                 { new Contact { id = "noa", name = "noale", server = "localhost:7242", last = "hiii", lastdate = "12:14" },
@@ -25,7 +25,7 @@
         {
             id = "dani",
             nickname = "div",
-            password = "1",
+            password = "Nn123456",
             image = "https://bootdey.com/img/Content/avatar/avatar6.png",
             contacts = new List<Contact>
             { new Contact { id = "noa", name = "noale", server = "localhost:7242", last = "see you", lastdate = "14:15" }},
